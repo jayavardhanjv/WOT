f7c4876aaf10cf15b456fbb6b568edfff8e8f7b4
jayavardhan jv <jjayavardhan81@gmail.com>
2024-02-08 23:06:20 +0530

    slider changes

--- src/components/Movie_items.jsx
+++ src/components/Movie_items.jsx
@@ -8,6 +8,10 @@ export default function Movie_items() {
   const [loaded, setLoaded] = useState(false)
   const [sliderRef, instanceRef] = useKeenSlider({
     initial: 0,
+    slides: {
+      perView: 4,
+      spacing: 5,
+    },
     slideChanged(slider) {
       setCurrentSlide(slider.track.details.rel)
     },
@@ -18,16 +22,8 @@ export default function Movie_items() {
 
   return (
     <>
-      <div className="flex">
-        <div ref={sliderRef} className="keen-slider">
-          <div className="keen-slider__slide number-slide1">1</div>
-          <div className="keen-slider__slide number-slide2">2</div>
-          <div className="keen-slider__slide number-slide3">3</div>
-          <div className="keen-slider__slide number-slide4">4</div>
-          <div className="keen-slider__slide number-slide5">5</div>
-          <div className="keen-slider__slide number-slide6">6</div>
-        </div>
-        {loaded && instanceRef.current && (
+      <div className="flex w-full h-60">
+      {loaded && instanceRef.current && (
           <>
             <Arrow
               left
@@ -35,8 +31,23 @@ export default function Movie_items() {
                 e.stopPropagation() || instanceRef.current?.prev()
               }
               disabled={currentSlide === 0}
-              className="bg-white"
+              className="bg-white h-10 p-16"
             />
+
+
+          </>
+        )}
+        <div ref={sliderRef} className="keen-slider text-black gap-5 items-center">
+          <div className="keen-slider__slide number-slide1 bg-white ">1</div>
+          <div className="keen-slider__slide number-slide2 bg-white">2</div>
+          <div className="keen-slider__slide number-slide3 bg-white">3</div>
+          <div className="keen-slider__slide number-slide4 bg-white">4</div>
+          <div className="keen-slider__slide number-slide5 bg-white">5</div>
+          <div className="keen-slider__slide number-slide6 bg-white">6</div>
+        </div>
+        {loaded && instanceRef.current && (
+          <>
+
 
             <Arrow
               onClick={(e) =>
@@ -62,11 +73,13 @@ export default function Movie_items() {
                 onClick={() => {
                   instanceRef.current?.moveToIdx(idx)
                 }}
-                className={"dot" + (currentSlide === idx ? " active" : "")}
+                class={"dot" + (currentSlide === idx ? " active" : "")}
+                className='fill-black'
               ></button>
             )
           })}
         </div>
+        
       )}
     </>
   )
@@ -84,10 +97,10 @@ function Arrow(props) {
       viewBox="0 0 24 24"
     >
       {props.left && (
-        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+        <path className='fill-white justify-start ' d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
       )}
       {!props.left && (
-        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+        <path className='fill-white justify-end ' d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
       )}
     </svg>
   )
